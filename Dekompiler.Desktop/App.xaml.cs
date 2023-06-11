7889e62906f6b0ea60d74ca60b44353d04d6fd56
CursedLand <cursedland@protonmail.com>
2023-06-12 02:18:16 +0300

    Add Desktop version project.

---
+++ Dekompiler.Desktop/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dekompiler.Desktop {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        private void StartupSetup(object sender, StartupEventArgs e) {
+            AppDomain.CurrentDomain.UnhandledException += (_, error) => {
+                MessageBox.Show(error.ExceptionObject.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+        }
+    }
+}
---
+++ Dekompiler.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Dekompiler.Core;
+using Dekompiler.Core.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dekompiler.Desktop
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var serviceCollection = new ServiceCollection();
+			serviceCollection.AddWpfBlazorWebView();
+			serviceCollection.AddSingleton(ThemeConfiguration.Default); // change to .Dark to get dark-theme.
+			serviceCollection.AddSingleton<RendererService>();
+			Resources.Add("services", serviceCollection.BuildServiceProvider());
+		}
+	}
+}
